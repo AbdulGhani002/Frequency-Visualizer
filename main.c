b285c28113ec16f6025dbcd63bfcf0c111c9689c
AbdulGhani002 <itsaghani@gmail.com>
2025-03-05 22:01:00 +0500

    Volume up and down functionality is added

--- main.c
+++ main.c
@@ -1,29 +1,68 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <raylib.h>
 
+void callback (void* bufferData, unsigned int frames){
+	(void) bufferData;
+	(void) frames;
+	printf("Callback Frames: %u\n", frames);
+}
+
 int main(void){
-	InitWindow(800,600,"Frequency Visualizer");
+	freopen("output.log", "w", stdout);
+	InitWindow(800, 600, "Frequency Visualizer");
 	SetTargetFPS(90);
-	InitAudioDevice();
+    	InitAudioDevice();
 
-	Music music = LoadMusicStream("./assets/alone.mp3");
-	PlayMusicStream(music);
+    	Music music = LoadMusicStream("./assets/alone.mp3");
+    	PlayMusicStream(music);
+    	AttachAudioStreamProcessor(music.stream,callback);
 
-	while(!WindowShouldClose()){
 
-		if(IsKeyPressed(KEY_SPACE)){
-			if(IsMusicStreamPlaying(music)){
-				PauseMusicStream(music);
-			} else {
-				ResumeMusicStream(music);
-				}
-}
+    	float volume = 0.5f; // Storing volume separately
+    	SetMusicVolume(music, volume); 
 
-		UpdateMusicStream(music);
-		BeginDrawing();
-		ClearBackground(GREEN);
-		EndDrawing();
-}
-	return 0;
+    	while (!WindowShouldClose()) {
+    
+        	if (IsKeyPressed(KEY_SPACE)) {
+        
+            		if (IsMusicStreamPlaying(music)) {
+                		PauseMusicStream(music);
+            		} else {
+                		ResumeMusicStream(music);
+            		}
+        	}
+
+        if (IsKeyPressed(KEY_UP)) {
+        	volume += 0.1f;
+            	if (volume > 1.0f){ 
+            		volume = 1.0f;
+            	} // Max Value of volume that can be passed is 1
+            	SetMusicVolume(music, volume);
+        }
+        
+        if (IsKeyPressed(KEY_DOWN)) {
+            	volume -= 0.1f;
+            	if (volume < 0.0f){ 
+            		volume = 0.0f; // Minimum value of volume that can be passed is 0 
+            	}
+            	SetMusicVolume(music, volume);
+        }
+
+        UpdateMusicStream(music);
+        
+        BeginDrawing();
+        ClearBackground(GREEN);
+        DrawText("Press SPACE to Play/Pause", 20, 20, 20, WHITE);
+        DrawText("Press UP to Increase Volume", 20, 50, 20, WHITE);
+        DrawText("Press DOWN to Decrease Volume", 20, 80, 20, WHITE);
+        DrawText(TextFormat("Volume: %.2f", volume), 20, 110, 20, WHITE);
+        EndDrawing();
+    }
+
+    // Cleaning up resources
+    UnloadMusicStream(music);
+    CloseAudioDevice();
+    CloseWindow();
+    fflush(stdout);
+    return 0;
 }
